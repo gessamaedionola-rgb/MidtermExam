69a7bf48dd74c5a0a987445a1bf46a8602bbfde2
gessamaedionola-rgb <gessamaedionola@gmail.com>
2026-04-24 09:16:42 +0800

    part3

---
+++ student.js
@@ -0,0 +1,14 @@
+// Student Information
+const student = {
+    name: "Dionola GessaMae",
+    course: "BS Information Technology",
+    yearLevel: "3rd Year",
+    favoriteProgrammingLanguage: "JavaScript"
+};
+
+// Display student information
+console.log("=== Student Information ===");
+console.log("Name: " + student.name);
+console.log("Course: " + student.course);
+console.log("Year Level: " + student.yearLevel);
+console.log("Favorite Programming Language: " + student.favoriteProgrammingLanguage);
